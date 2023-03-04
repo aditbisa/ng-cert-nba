d3a2164c2fc38e0de2e6ae7d9daf3939e76e6d13
Aditya Ghaffar <adit.bisa@gmail.com>
2023-03-04 09:02:24 +0700

    refactor: move service call from template

--- src/app/game-stats/game-stats.component.ts
+++ src/app/game-stats/game-stats.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
-import {Team} from '../data.models';
-import {Observable, tap} from 'rxjs';
-import {NbaService} from '../nba.service';
+import { Observable, tap } from 'rxjs';
+
+import { Team } from '../data.models';
+import { NbaService } from '../nba.service';
 
 @Component({
   selector: 'app-game-stats',
   templateUrl: './game-stats.component.html',
-  styleUrls: ['./game-stats.component.css']
+  styleUrls: ['./game-stats.component.css'],
 })
 export class GameStatsComponent {
-
   teams$: Observable<Team[]>;
   allTeams: Team[] = [];
 
   constructor(protected nbaService: NbaService) {
-    this.teams$ = nbaService.getAllTeams().pipe(
-      tap(data => this.allTeams = data)
-    );
+    this.teams$ = nbaService
+      .getAllTeams()
+      .pipe(tap((data) => (this.allTeams = data)));
   }
 
   trackTeam(teamId: string): void {
-    let team = this.allTeams.find(team => team.id == Number(teamId));
-    if (team)
-      this.nbaService.addTrackedTeam(team);
+    let team = this.allTeams.find((team) => team.id == Number(teamId));
+    if (team) this.nbaService.addTrackedTeam(team);
+  }
+
+  getTrackedTeams(): Team[] {
+    return this.nbaService.getTrackedTeams();
   }
 }
--- src/app/team-stats/team-stats.component.ts
+++ src/app/team-stats/team-stats.component.ts
@@ -1,26 +1,34 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Observable, tap} from 'rxjs';
-import {NbaService} from '../nba.service';
-import {Game, Stats, Team} from '../data.models';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+
+import { Game, Stats, Team } from '../data.models';
+import { NbaService } from '../nba.service';
 
 @Component({
   selector: 'app-team-stats',
   templateUrl: './team-stats.component.html',
-  styleUrls: ['./team-stats.component.css']
+  styleUrls: ['./team-stats.component.css'],
 })
 export class TeamStatsComponent implements OnInit {
-
   @Input()
   team!: Team;
 
   games$!: Observable<Game[]>;
   stats!: Stats;
-  constructor(protected nbaService: NbaService) { }
+  constructor(protected nbaService: NbaService) {}
 
   ngOnInit(): void {
-    this.games$ = this.nbaService.getLastResults(this.team, 12).pipe(
-      tap(games =>  this.stats = this.nbaService.getStatsFromGames(games, this.team))
-    )
+    this.games$ = this.nbaService
+      .getLastResults(this.team, 12)
+      .pipe(
+        tap(
+          (games) =>
+            (this.stats = this.nbaService.getStatsFromGames(games, this.team))
+        )
+      );
   }
 
+  removeTrackedTeam(team: Team) {
+    this.nbaService.removeTrackedTeam(team);
+  }
 }
