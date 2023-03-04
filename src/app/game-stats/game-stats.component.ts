650cecb3638d9237c44f8c38599c8bdbef31a63a
Aditya Ghaffar <adit.bisa@gmail.com>
2023-03-04 23:30:35 +0700

    feat: filter teams

--- src/app/game-stats/game-stats.component.ts
+++ src/app/game-stats/game-stats.component.ts
@@ -10,25 +10,55 @@ import { NbaService } from '../nba.service';
   styleUrls: ['./game-stats.component.css'],
 })
 export class GameStatsComponent {
-  teams$: Observable<Team[]>;
   allTeams: Team[] = [];
   allConferences: string[] = [];
-  allDivisions: string[] = [];
+
+  loading = true;
+  conference = '';
+  division = '';
+  filteredDivisions: string[] = [];
+  filteredTeams: Team[] = [];
 
   constructor(protected nbaService: NbaService) {
-    this.teams$ = nbaService.getAllTeams().pipe(
-      tap((data) => {
-        this.allTeams = data;
-        this.allConferences = data
-          .map((team) => team.conference)
-          .filter((v, i, a) => a.indexOf(v) === i);
-        this.allDivisions = data
-          .map((team) => team.division)
-          .filter((v, i, a) => a.indexOf(v) === i);
-        console.log(this.allConferences);
-        console.log(this.allDivisions);
-      })
-    );
+    nbaService.getAllTeams().subscribe((data) => {
+      this.allTeams = data;
+      this.allTeams.sort((a, b) => {
+        if (a.full_name < b.full_name) return -1;
+        if (a.full_name > b.full_name) return 1;
+        return 0;
+      });
+
+      this.allConferences = data
+        .map((team) => team.conference)
+        .filter((v, i, a) => a.indexOf(v) === i);
+      this.allConferences.sort();
+
+      this.loading = false;
+      this.filterByConference();
+    });
+  }
+
+  filterByConference() {
+    this.filteredDivisions = this.allTeams
+      .filter((team) =>
+        this.conference ? team.conference == this.conference : true
+      )
+      .map((team) => team.division)
+      .filter((v, i, a) => a.indexOf(v) === i);
+
+    if (!this.filteredDivisions.includes(this.division)) {
+      this.division = '';
+      this.filterByDivision();
+    }
+  }
+
+  filterByDivision() {
+    this.filteredTeams = this.allTeams.filter((team) => {
+      let check = true;
+      if (this.conference) check = check && team.conference == this.conference;
+      if (this.division) check = check && team.division == this.division;
+      return check;
+    });
   }
 
   trackTeam(teamId: string): void {
